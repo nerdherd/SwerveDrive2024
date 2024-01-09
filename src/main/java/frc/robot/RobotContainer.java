354d8720affe30ca77400554d46442e3ed9624bc
Kyle Huang <kylehuang06@gmail.com>
2024-01-09 13:29:19 -0800

    Revert "Temp comment out all teleop"
    This reverts commit 87e61ebabd501df11108ef8e43bc77473193ff16.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,29 +4,29 @@
 
 package frc.robot;
 
-// import java.time.Instant;
+import java.time.Instant;
 import java.util.function.Supplier;
 
-// import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
-// import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
-// import edu.wpi.first.wpilibj2.command.InstantCommand;
-// import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
-// import edu.wpi.first.wpilibj2.command.button.POVButton;
-// import edu.wpi.first.wpilibj2.command.button.Trigger;
-// import frc.robot.Constants.ControllerConstants;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.ControllerConstants;
 import frc.robot.Constants.VisionConstants;
-// import frc.robot.commands.SwerveJoystickCommand;
-// import frc.robot.commands.SwerveJoystickCommand.DodgeDirection;
+import frc.robot.commands.SwerveJoystickCommand;
+import frc.robot.commands.SwerveJoystickCommand.DodgeDirection;
 // import frc.robot.commands.VisionAutos.ToNearestGridDebug;
 import frc.robot.commands.autos.PathPlannerAutos;
 import frc.robot.commands.autos.SquareTest;
@@ -54,25 +54,25 @@ public class RobotContainer {
   public SwerveDrivetrain swerveDrive;
   public PowerDistribution pdp = new PowerDistribution(0, ModuleType.kCTRE);
 
-  // private final CommandPS4Controller commandDriverController = new CommandPS4Controller(
-  //     ControllerConstants.kDriverControllerPort);
-  // private final PS4Controller driverController = commandDriverController.getHID();
-  // // Replace with CommandPS4Controller or CommandJoystick if needed
-  // private final CommandPS4Controller commandOperatorController = new CommandPS4Controller(
-  //     ControllerConstants.kOperatorControllerPort);
-  // private final PS4Controller operatorController = commandOperatorController.getHID();
-  // // private final Joystick joystick = new Joystick(2);
+  private final CommandPS4Controller commandDriverController = new CommandPS4Controller(
+      ControllerConstants.kDriverControllerPort);
+  private final PS4Controller driverController = commandDriverController.getHID();
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandPS4Controller commandOperatorController = new CommandPS4Controller(
+      ControllerConstants.kOperatorControllerPort);
+  private final PS4Controller operatorController = commandOperatorController.getHID();
+  // private final Joystick joystick = new Joystick(2);
 
   private final LOG_LEVEL loggingLevel = LOG_LEVEL.MINIMAL;
-  // private final POVButton upButton = new POVButton (operatorController,0);
-  // private final POVButton rightButton = new POVButton (operatorController, 90);
-  // private final POVButton downButton = new POVButton (operatorController, 180);
-  // private final POVButton leftButton = new POVButton (operatorController, 270);
+  private final POVButton upButton = new POVButton (operatorController,0);
+  private final POVButton rightButton = new POVButton (operatorController, 90);
+  private final POVButton downButton = new POVButton (operatorController, 180);
+  private final POVButton leftButton = new POVButton (operatorController, 270);
 
-  // private final POVButton upButtonDriver = new POVButton (driverController, 0);
-  // private final POVButton rightButtonDriver = new POVButton (driverController, 90);
-  // private final POVButton downButtonDriver = new POVButton (driverController, 180);
-  // private final POVButton leftButtonDriver = new POVButton (driverController, 270);
+  private final POVButton upButtonDriver = new POVButton (driverController, 0);
+  private final POVButton rightButtonDriver = new POVButton (driverController, 90);
+  private final POVButton downButtonDriver = new POVButton (driverController, 180);
+  private final POVButton leftButtonDriver = new POVButton (driverController, 270);
 
   private SendableChooser<Supplier<CommandBase>> autoChooser = new SendableChooser<Supplier<CommandBase>>();
 
@@ -93,69 +93,69 @@ public class RobotContainer {
     initShuffleboard();
 
     // Configure the trigger bindings
-    // configureBindings();
+    configureBindings();
 
     DriverStation.reportWarning("Initalization complete", false);
   }
 
-  // public void initDefaultCommands() {
-  //   swerveDrive.setDefaultCommand(
-  //     new SwerveJoystickCommand(
-  //       swerveDrive,
-  //       () -> -commandDriverController.getLeftY(), // Horizontal translation
-  //       commandDriverController::getLeftX, // Vertical Translation
-  //       // () -> 0.0, // debug
-  //       commandDriverController::getRightX, // Rotationaq
+  public void initDefaultCommands() {
+    swerveDrive.setDefaultCommand(
+      new SwerveJoystickCommand(
+        swerveDrive,
+        () -> -commandDriverController.getLeftY(), // Horizontal translation
+        commandDriverController::getLeftX, // Vertical Translation
+        // () -> 0.0, // debug
+        commandDriverController::getRightX, // Rotationaq
 
-  //       // driverController::getTriangleButton, // Field oriented
-  //       () -> false, // Field oriented
+        // driverController::getTriangleButton, // Field oriented
+        () -> false, // Field oriented
 
-  //       driverController::getCrossButton, // Towing
-  //       // driverController::getR2Button, // Precision/"Sniper Button"
-  //       () -> driverController.getR2Button(), // Precision mode (disabled)
-  //       () -> driverController.getCircleButton(), // Turn to angle
-  //       // () -> false, // Turn to angle (disabled)
-  //       () -> { // Turn To angle Direction
-  //         return 0.0;
-  //       }
-  //     ));
+        driverController::getCrossButton, // Towing
+        // driverController::getR2Button, // Precision/"Sniper Button"
+        () -> driverController.getR2Button(), // Precision mode (disabled)
+        () -> driverController.getCircleButton(), // Turn to angle
+        // () -> false, // Turn to angle (disabled)
+        () -> { // Turn To angle Direction
+          return 0.0;
+        }
+      ));
 
-  //     // Removed to allow percent output joystick control
-  //     // wrist.setDefaultCommand(Commands.run(() -> wrist.moveWristMotionMagicJoystick(-operatorController.getLeftY()), wrist));
-  // }
+      // Removed to allow percent output joystick control
+      // wrist.setDefaultCommand(Commands.run(() -> wrist.moveWristMotionMagicJoystick(-operatorController.getLeftY()), wrist));
+  }
 
-  // private void configureBindings() {
-  //   // Note: whileTrue() does not restart the command if it ends while the button is
-  //   // still being held
-  //   // These button bindings are chosen for testing, and may be changed based on
-  //   commandDriverController.share().onTrue(Commands.runOnce(imu::zeroHeading).andThen(() -> imu.setOffset(0)));
-  //   commandDriverController.options().onTrue(Commands.runOnce(swerveDrive::resetEncoders));
-  //   // commandDriverController.options().onTrue(Commands.runOnce(wrist::resetEncoders));
-  //   // commandDriverController.PS().whileTrue(new TheGreatBalancingAct(swerveDrive));
+  private void configureBindings() {
+    // Note: whileTrue() does not restart the command if it ends while the button is
+    // still being held
+    // These button bindings are chosen for testing, and may be changed based on
+    commandDriverController.share().onTrue(Commands.runOnce(imu::zeroHeading).andThen(() -> imu.setOffset(0)));
+    commandDriverController.options().onTrue(Commands.runOnce(swerveDrive::resetEncoders));
+    // commandDriverController.options().onTrue(Commands.runOnce(wrist::resetEncoders));
+    // commandDriverController.PS().whileTrue(new TheGreatBalancingAct(swerveDrive));
 
-  //   // commandDriverController.cross().whileTrue(Commands.run(() -> swerveDrive.driveToSunflower(4, 5)));
+    // commandDriverController.cross().whileTrue(Commands.run(() -> swerveDrive.driveToSunflower(4, 5)));
 
-  //   // commandDriverController.triangle().whileTrue(new TheGreatBalancingAct(swerveDrive));
-  //   commandDriverController.circle()
-  //     .onTrue(Commands.runOnce(() -> swerveDrive.setVelocityControl(true)))
-  //     .onFalse(Commands.runOnce(() -> swerveDrive.setVelocityControl(false)));
+    // commandDriverController.triangle().whileTrue(new TheGreatBalancingAct(swerveDrive));
+    commandDriverController.circle()
+      .onTrue(Commands.runOnce(() -> swerveDrive.setVelocityControl(true)))
+      .onFalse(Commands.runOnce(() -> swerveDrive.setVelocityControl(false)));
     
     
-  //   // Note:
-  //   // L2:  hold = intake     let go = stow + hold
-  //   // L1:  press = aim low   let go = score + stow
-  //   // R1:  press = aim mid   let go = score + stow
-  //   // R2:  press = aim high  let go = score + stow
+    // Note:
+    // L2:  hold = intake     let go = stow + hold
+    // L1:  press = aim low   let go = score + stow
+    // R1:  press = aim mid   let go = score + stow
+    // R2:  press = aim high  let go = score + stow
 
-  //   // commandOperatorController.square()
-  //   //   .onTrue(temp += 10)
-  //   //   .onFalse(temp -= 10);
+    // commandOperatorController.square()
+    //   .onTrue(temp += 10)
+    //   .onFalse(temp -= 10);
 
-  //   // commandOperatorController.square()
-  //   //   .onTrue(new InstantCommand(() -> new ToNearestGridDebug(swerveDrive, sunflower)));
-  //   // commandOperatorController.cross()
-  //   //   .onTrue(new InstantCommand(() -> sunflower.toNearestGrid()));
-  // }
+    // commandOperatorController.square()
+    //   .onTrue(new InstantCommand(() -> new ToNearestGridDebug(swerveDrive, sunflower)));
+    // commandOperatorController.cross()
+    //   .onTrue(new InstantCommand(() -> sunflower.toNearestGrid()));
+  }
 
   private void initAutoChoosers() {
     // Remember to load the pathplanner paths here
