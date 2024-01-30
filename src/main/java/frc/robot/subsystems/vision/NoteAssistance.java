84adc9dc270f58b36e0ee272909787462d293297
Yajing Duan <yajingduan@gmail.com>
2024-01-30 00:28:22 -0800

    clean up

--- src/main/java/frc/robot/subsystems/vision/NoteAssistance.java
+++ src/main/java/frc/robot/subsystems/vision/NoteAssistance.java
@@ -2,15 +2,14 @@ package frc.robot.subsystems.vision;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants.VisionConstants;
 import frc.robot.subsystems.Reportable;
 import frc.robot.subsystems.swerve.SwerveDrivetrain;
+import frc.robot.subsystems.vision.Limelight.LightMode;
 import frc.robot.util.NerdyMath;
 
 public class NoteAssistance implements Reportable{
@@ -34,7 +33,6 @@ public class NoteAssistance implements Reportable{
         this.name = name;
         ShuffleboardTab tab = Shuffleboard.getTab(name);
 
-        //TODO set pid constants to preferences for easy tuning
         areaController = new PIDController(0.34, 0, 0);// todo, tuning pls!!!
         txController = new PIDController(0.08, 0, 0.006);// todo, tuning pls!!!
 
@@ -49,7 +47,7 @@ public class NoteAssistance implements Reportable{
         }
     }
 
-    public void pidTuning_test() {
+    private void pidTuning_test() {
         VisionConstants.kPNoteForward.loadPreferences();
         VisionConstants.kINoteForward.loadPreferences();
         VisionConstants.kDNoteForward.loadPreferences();
@@ -71,7 +69,7 @@ public class NoteAssistance implements Reportable{
         dataSampleCount = 0;
     }
 
-    public void speedToNote(double targetArea, double targetTX, double targetTY) {
+    private void speedToNote(double targetArea, double targetTX, double targetTY) {
         if(limelight == null) return; // todo, never happens
         
         boolean hasTarget = limelight.hasValidTarget();
@@ -93,11 +91,11 @@ public class NoteAssistance implements Reportable{
 
             if( area < 0.5 || area > 5.5 ) // todo, tuning pls!!!
             {
-                speeds[0] = speeds[1] = 0; // something is wrong, or filted out by camera dashboard
+                speeds[0] = speeds[1] = 0; // something is wrong! or filter it out by camera dashboard
             } 
             else if( tx < 6 && tx > -6 && area > 3.7 ) // todo, tuning pls!!!
             {
-                speeds[0] = speeds[1] = 0; // good range to get the note. faster than the pid
+                speeds[0] = speeds[1] = 0; // good ranges to get the note. faster than the pid
             } 
             else if( ty < targetTY ) // need a lot of testing here
             {
@@ -123,16 +121,18 @@ public class NoteAssistance implements Reportable{
             sidewaysSpeed.setDouble(speeds[1]);
     }
 
+    // no time limit if MaxSamples is less than 0
     public void driveToNote(SwerveDrivetrain drivetrain, double targetArea, double targetTX, double targetTY, int maxSamples) {
-        // must reset before or after call this function!!!
+        // must reset counts before or after call this function!!!
         dataSampleCount++;
         speedToNote(targetArea, targetTX, targetTY);
         if(maxSamples > 0 && dataSampleCount > maxSamples)
-            drivetrain.drive(0, 0, 0);
-        else
-            drivetrain.drive(getForwardSpeed(), getSidewaysSpeed(), 0);
+            speeds[0] = speeds[1] = 0;
+            
+        drivetrain.drive(getForwardSpeed(), getSidewaysSpeed(), 0);
     }
 
+    // a min running time is required by minSamples for the auto command calling
     public Command driveToNoteCommand(SwerveDrivetrain drivetrain, double targetArea, int minSamples, int maxSamples) {
         return Commands.sequence(
             Commands.runOnce(() -> reset()),
@@ -140,13 +140,17 @@ public class NoteAssistance implements Reportable{
                 () -> driveToNote(drivetrain, targetArea, 0, 0.1, maxSamples)// todo, tuning pls!!!
             ).until(() -> (dataSampleCount >= minSamples && 
                 Math.abs(getForwardSpeed()) <= 0.1 && 
-                Math.abs(getSidewaysSpeed()) <= 0.1) ||// todo, tuning pls!!!
-                dataSampleCount > maxSamples)
+                Math.abs(getSidewaysSpeed()) <= 0.1) )// todo, tuning pls!!!
         );
     }
 
-    public double getForwardSpeed() { return speeds[0]; }
-    public double getSidewaysSpeed() { return speeds[1]; }
+    private double getForwardSpeed() { return speeds[0]; }
+    private double getSidewaysSpeed() { return speeds[1]; }
+
+    public void setLight(boolean lightModeOn) {
+        if(lightModeOn) limelight.setLightState(LightMode.ON);
+        else limelight.setLightState(LightMode.OFF);
+    }
 
     @Override
     public void reportToSmartDashboard(LOG_LEVEL priority) {
